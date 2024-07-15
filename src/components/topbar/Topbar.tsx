4dbf3ee1ce31d46b63df24f254c8ef1ef3420787
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2024-07-14 21:14:01 -0300

    Começando topbar

---
+++ src/components/topbar/Topbar.tsx
@@ -0,0 +1,23 @@
+import './Topbar.scss';
+
+
+export const Topbar = () => {
+    return (
+        <>
+            <header className="topbar">
+                <div>
+                    <span>Sábado: 9:00 AM - 5:30 PM <i>icon</i></span>
+                    <p>Deseja receber avisos de cupons e descontos ? <span>Contato-nos</span></p>
+                    <div >
+                        <i className='twitter' />
+                        <i className='facebook' />
+                        <i className='instagram' />
+                    </div>
+                </div>
+                <nav>
+
+                </nav>
+            </header>
+        </>
+    );
+};
